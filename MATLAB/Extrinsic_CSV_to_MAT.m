36ba805df277a282d28bebb11a9c9b96c5b03a80
Dhruv Sharma <42878758+shardrv@users.noreply.github.com>
2020-09-08 16:06:29 -0600

    Update Extrinsic_CSV_to_MAT.m

--- MATLAB/Extrinsic_CSV_to_MAT.m
+++ MATLAB/Extrinsic_CSV_to_MAT.m
@@ -1,1 +1,42 @@
+filename = 'obj2c_';
 
+for i=1:11   
+    
+        [demos{i}.pos] = csvimport([filename, num2str(i),  ... 
+            '/_slash_my_gen3_slash_base_feedback.csv'], ... 
+                                 'columns', {'thetax', 'thetay', 'thetaz'})';
+                             
+        [posdemos{i}.pos] = csvimport([filename,num2str(i), '/_slash_my_gen3_slash_base_feedback.csv'], ... 
+                                 'columns', {'thetax', 'thetay', 'thetaz'})';                    
+        [veldemos{i}.vel] = csvimport([filename,num2str(i), '/_slash_my_gen3_slash_base_feedback.csv'], ... 
+                                 'columns', {'thetax', 'thetay', 'thetaz'})';                   
+    nb_knots = 20;
+    nb_clean_data_per_demo = 100;
+
+    data = demos{i}.pos;
+    t = [1:size((demos{i}.pos),2)]*0.05;
+    
+    if(i>1)
+        if(t > (size((demos{i-1}.pos),2)))
+            t = size((demos{i-1}.pos),2);
+        end
+    end
+    nb_data = size(data,2);
+    skip = round(nb_data/100);
+    knots = data(:,1:skip:end);
+    knots = knots(:,1:90);
+    tt = t(:,1:skip:end);
+    tt = tt(:,1:90);
+    ppx = spline(tt,knots);
+    
+    % % and get first derivative
+    ppxd = differentiate_spline(ppx);
+    pos = ppval(ppx,tt);
+    vel = ppval(ppxd,tt);
+    
+    posdemos{i}.pos = pos;
+    veldemos{i}.vel = vel;
+   
+end
+
+save z_traj_kinova_obj2
